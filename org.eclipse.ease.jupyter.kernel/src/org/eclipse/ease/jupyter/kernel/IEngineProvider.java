1d5e30daceb246c19c18c03ba9d9c4f0c142a047
Martin Kloesch <martin.kloesch@gmail.com>
2018-06-21 13:56:35 +0200

    adds license header
    Signed-off-by: Martin Kloesch <martin.kloesch@gmail.com>

--- org.eclipse.ease.jupyter.kernel/src/org/eclipse/ease/jupyter/kernel/IEngineProvider.java
+++ org.eclipse.ease.jupyter.kernel/src/org/eclipse/ease/jupyter/kernel/IEngineProvider.java
@@ -1,4 +1,15 @@
-package org.eclipse.ease.jupyter.kernel;
+/*******************************************************************************
+ * Copyright (c) 2018 Martin Kloesch and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Martin Kloesch - initial API and implementation
+ *******************************************************************************/
+
+ package org.eclipse.ease.jupyter.kernel;
 
 import org.eclipse.ease.IScriptEngine;
 
